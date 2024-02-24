90b266b09a8aa073f307127d3d49eacfcf7a59dd
Mar Garza C <mariana.garzacd@uanl.edu.mx>
2024-02-23 21:18:33 -0600

    actualización

--- FrontEnd/astrato-app/src/App.jsx
+++ FrontEnd/astrato-app/src/App.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { useState } from 'react';
-import './App.css';
-import "@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css";
-import {MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
+import "./App.css";
+import "@chatscope/chat-ui-kit-react";
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
+
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +12,7 @@ function App() {
   return (
     <NextUIProvider>
         <div className="App">
-      
+
        </div>
   </NextUIProvider>
   );
