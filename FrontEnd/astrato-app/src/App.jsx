0761e9141e983bcb9a22ad79ab5467bf0354a15f
Mar Garza C <mariana.garzacd@uanl.edu.mx>
2024-02-23 23:03:47 -0600

    CARD

--- FrontEnd/astrato-app/src/App.jsx
+++ FrontEnd/astrato-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardBody } from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 import "./App.css";
 
 function App() {
@@ -7,14 +7,18 @@ function App() {
 
   return (
     <div className="app-container">
-
       <div className="image-container">
         <img
           src="src/img/atrato.png"
           alt="Descripción de la imagen"
-          className="rounded-image"
-        />
+          className="rounded-image"/>
       </div>
+    <Card>
+      <CardBody>
+        <p>Make beautiful websites regardless of your design experience.</p>
+      </CardBody>
+    </Card>
+
     </div>
   );
 }
