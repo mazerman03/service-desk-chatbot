17eef684a12ab3e60747789cfcf6ad8df8153522
Mar Garza C <mariana.garzacd@uanl.edu.mx>
2024-02-23 20:58:52 -0600

    Update App.jsx

--- FrontEnd/astrato-app/src/App.jsx
+++ FrontEnd/astrato-app/src/App.jsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
 import './App.css';
 import "@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css";
 import {MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
