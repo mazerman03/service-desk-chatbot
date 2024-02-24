75cbbc2165b885623db369447bed7e770456a1e2
Mar Garza C <mariana.garzacd@uanl.edu.mx>
2024-02-23 20:55:33 -0600

    nextUI

--- FrontEnd/astrato-app/src/App.jsx
+++ FrontEnd/astrato-app/src/App.jsx
@@ -1,14 +1,20 @@
+import * as React from "react";
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import './App.css';
+import "@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css";
+import {MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from "@chatscope/chat-ui-kit-react"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-    </div>
+    <NextUIProvider>
+        <div className="App">
+      
+       </div>
+  </NextUIProvider>
   );
 }
 
