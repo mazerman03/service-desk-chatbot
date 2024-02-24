bd3d9542ea9eabfa2e7ea9a9f6558d2aa398c6dc
Mar Garza C <mariana.garzacd@uanl.edu.mx>
2024-02-23 20:40:45 -0600

    astrato-app file created

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
@@ -7,3 +7,15 @@ const root = ReactDOM.createRoot(document.getEelementById("root"));
 root.render(
     <App/>
 )
+
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
+
+function App() {
+  // 2. Wrap NextUIProvider at the root of your app
+  return (
+    <NextUIProvider>
+      <YourApplication />
+    </NextUIProvider>
+  );
+}
