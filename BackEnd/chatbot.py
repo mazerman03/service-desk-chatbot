11f5171e2e332544a74568d559d9b86d05fe3660
Juanjo H. Beltrán <a00836747@tec.mx>
2024-02-24 01:22:11 -0600

    Update

--- BackEnd/chatbot.py
+++ BackEnd/chatbot.py
@@ -10,19 +10,19 @@ from keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 
-#Importamos los archivos generados en el código anterior
+# Importamos los archivos generados en el código anterior
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.keras')
 
-#Pasamos las palabras de oración a su forma raíz
+# Pasamos las palabras de oración a su forma raíz
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
-#Convertimos la información a unos y ceros según si están presentes en los patrones
+# Convertimos la información a unos y ceros según si están presentes en los patrones
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0]*len(words)
@@ -30,10 +30,10 @@ def bag_of_words(sentence):
         for i, word in enumerate(words):
             if word == w:
                 bag[i]=1
-    print(bag)
+    # print(bag)
     return np.array(bag)
 
-#Predecimos la categoría a la que pertenece la oración
+# Predecimos la categoría a la que pertenece la oración
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
@@ -51,7 +51,7 @@ def get_response(tag, intents_json):
             break
     return result
 
-#Ejecutamos el chat en bucle
+# Ejecutamos el chat en bucle
 while True:
     message=input("")
     ints = predict_class(message)
